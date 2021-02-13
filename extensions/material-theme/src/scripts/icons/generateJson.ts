de732e57300b53d66ce6341cfe1087289bf6b498
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-14 05:39:00 +0900

    init for material theme (wip)

---
+++ extensions/material-theme/src/scripts/icons/generateJson.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is meant to be executed exclusively by npm scripts.
+ */
+import { createIconFile } from './../../icons/index';
+
+try {
+  createIconFile();
+} catch (error) {
+  throw Error(error);
+}
